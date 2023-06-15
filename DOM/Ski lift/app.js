192caba217281c82e6f58ec0e31fe7122c366b73
Iliyan Ivanov <109862097+oldbasic@users.noreply.github.com>
2023-06-15 12:24:54 +0300

    improved readability -> Ski lift exercise

--- DOM/Ski lift/app.js
+++ DOM/Ski lift/app.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', solve);
 
 function solve() {
-	//*capture elements
+	//*Select form data
 	const formData = {
 		firstName: document.querySelector('#first-name'),
 		lastName: document.querySelector('#last-name'),
@@ -9,22 +9,24 @@ function solve() {
 		fromDate: document.querySelector('#from-date'),
 		daysCount: document.querySelector('#days-count'),
 	};
+
+	//*Select static elements
 	const nextStepBtn = document.querySelector('#next-btn');
 	const ticketInfoList = document.querySelector('.ticket-info-list');
 	const confirmTicketList = document.querySelector('.confirm-ticket');
 
-	//* Add handlers to the button
+	//* Add handler to the button
 	nextStepBtn.addEventListener('click', nextStepBtnHandler);
 
 	//* Helper functions()
 	function createTicket(data) {
-		//*handle data
+		//*Handle data
 		const name = `Name: ${data[0].value} ${data[1].value}`;
 		const date = `From date: ${data[3].value}`;
 		const days = `For ${data[4].value} days`;
 		const people = `For ${data[2].value} people`;
 
-		//*create
+		//*Create
 		const li = document.createElement('li');
 		const article = document.createElement('article');
 		const h3 = document.createElement('h3');
@@ -34,7 +36,7 @@ function solve() {
 		const editBtn = document.createElement('button');
 		const continueBtn = document.createElement('button');
 
-		//*add properties
+		//*Modify
 		li.classList.add('ticket');
 		h3.textContent = name;
 		dateParagraph.textContent = date;
@@ -47,7 +49,7 @@ function solve() {
 		continueBtn.textContent = 'Continue';
 		continueBtn.addEventListener('click', continueBtnHandler);
 
-		//*attach
+		//*Attach
 		article.appendChild(h3);
 		article.appendChild(dateParagraph);
 		article.appendChild(daysParagraph);
@@ -59,29 +61,34 @@ function solve() {
 		return li;
 	}
 
-	//*ticketStorage
-
 	//* Event handlers
 	function nextStepBtnHandler(e) {
 		e.preventDefault();
-		//data validation
+
+		//*Data validation
 		const data = Object.values(formData);
 		const emptyInputField = data.find((input) => input.value === '');
 		if (emptyInputField) {
-			console.log('Empty filed detected!');
+			console.log('Empty input detected!');
 			return;
 		}
-		//attach new ticket
+
+		//*Attach
 		const ticket = createTicket(data);
 		ticketInfoList.appendChild(ticket);
-		//clear inputs
+
+		//*Clear inputs
 		data.forEach((input) => (input.value = ''));
-		//disable nextBtn
+
+		//*Disable nextBtn
 		e.target.disabled = true;
 	}
 	function editBtnHandler(e) {
+		//*Select
 		const li = e.target.parentElement;
 		const article = e.target.previousSibling;
+
+		//*Modify
 		const data = Array.from(article.children);
 		const name = data[0].textContent.substring(6);
 		const firstName = name.split(' ')[0];
@@ -90,28 +97,31 @@ function solve() {
 		const yy = dateParts[0].trim();
 		const mm = dateParts[1].trim();
 		const dd = dateParts[2].trim();
-
 		const days = data[2].textContent.split(' ')[1];
 		const people = data[3].textContent.split(' ')[1];
 
+		//*Update
 		formData.firstName.value = firstName;
 		formData.lastName.value = lastName;
 		formData.fromDate.value = `${yy}-${mm}-${dd}`;
 		formData.daysCount.value = days;
 		formData.numberOfPerople.value = people;
 
+		//*Remove
 		li.remove();
+
 		nextStepBtn.disabled = false;
 	}
 	function continueBtnHandler(e) {
+		//* Create
 		const li = document.createElement('li');
-
 		const article = ticketInfoList.querySelector(
 			'.ticket > article:nth-child(1)'
 		);
 		const confirmBtn = document.createElement('button');
 		const cancelBtn = document.createElement('button');
 
+		//* Modify
 		li.classList.add('ticket-content');
 		confirmBtn.classList.add('confirm-btn');
 		confirmBtn.textContent = 'Confirm';
@@ -120,32 +130,46 @@ function solve() {
 		cancelBtn.textContent = 'Cancel';
 		cancelBtn.addEventListener('click', cancelBtnHandler);
 
+		//*Attach
 		li.appendChild(article);
 		li.appendChild(confirmBtn);
 		li.appendChild(cancelBtn);
 		confirmTicketList.appendChild(li);
 
+		//*Remove
 		const removeLi = e.target.parentElement;
 		removeLi.remove();
 	}
 	function confirmBtnHandler(e) {
+		//*Select
 		const main = document.querySelector('#main');
-		main.remove();
 		const body = document.querySelector('#body');
+
+		//*Create
 		const h1 = document.createElement('h1');
+		const btn = document.createElement('button');
+
+		//*Modify
 		h1.id = 'thank-you';
 		h1.textContent = 'Thank you, have a nice day!';
-		const btn = document.createElement('button');
 		btn.id = 'back-btn';
 		btn.textContent = 'Back ';
 		btn.addEventListener('click', () => window.location.reload());
 
+		//*Attach
 		body.appendChild(h1);
 		body.appendChild(btn);
+
+		//*Remove
+		main.remove();
 	}
 	function cancelBtnHandler(e) {
+		//*Select
 		const li = e.target.parentElement;
+
+		//*Remove
 		li.remove();
+
 		nextStepBtn.disabled = false;
 	}
 }
